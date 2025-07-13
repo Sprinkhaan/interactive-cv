3477855a6c2bb74296557617059fe8a0df060282
Sprinkhaan <84741733+Sprinkhaan@users.noreply.github.com>
2025-07-13 13:41:35 +0200

    Initial commit with backend and frontend

---
+++ backend/cv-backend/src/test/java/ma/port/cvbackend/CvBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.port.cvbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
